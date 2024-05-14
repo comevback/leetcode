d46696d5e3148f715aa66cb3a17c90a9ce9da93f
xuxiang <xuxiang5012@gmail.com>
2024-05-15 00:07:18 +0900

    not finish 1219

---
+++ Code/not-finish-1219-Path-with-Maximum-Gold/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	grid := [][]int{{0, 6, 0}, {5, 8, 7}, {0, 9, 0}}
+
+}
+
+func getMaximumGold(grid [][]int) int {
+	//allGold := getSumGrid(grid)
+	return 0
+}
+
+func getSumGrid(grid [][]int) int {
+	sum := 0
+	for _, v := range grid {
+		for _, w := range v {
+			sum += w
+		}
+	}
+	return sum
+}
