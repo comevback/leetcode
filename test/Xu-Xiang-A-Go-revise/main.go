1fd24d72dd6b6656038e35ffe92761860d10dab0
xuxiang <xuxiang5012@gmail.com>
2024-05-16 15:15:50 +0900

    new file:   .gitignore 	new file:   Paiza/B/B029/B029.md 	new file:   test/Xu-Xiang-A-Go-revise.zip 	new file:   test/Xu-Xiang-A-Go-revise/main.go 	new file:   "test/Xu-Xiang-A-Go-revise/\343\202\263\343\203\274\343\203\207\343\202\243\343\203\263\343\202\260\343\202\271\343\202\255\343\203\253\343\203\201\343\202\247\343\203\203\343\202\257\343\200\200A\343\203\251\343\203\263\343\202\257 \357\274\210\350\220\275\343\201\241\343\202\202\343\201\256\343\202\267\343\203\237\343\203\245\343\203\254\343\203\274\343\202\267\343\203\247\343\203\263\357\274\211.pdf" 	new file:   test/Xu-Xiang-A-Go.zip 	new file:   test/Xu-Xiang-A-Go/main.go 	new file:   "test/Xu-Xiang-A-Go/\343\202\263\343\203\274\343\203\207\343\202\243\343\203\263\343\202\260\343\202\271\343\202\255\343\203\253\343\203\201\343\202\247\343\203\203\343\202\257\343\200\200A\343\203\251\343\203\263\343\202\257 \357\274\210\350\220\275\343\201\241\343\202\202\343\201\256\343\202\267\343\203\237\343\203\245\343\203\254\343\203\274\343\202\267\343\203\247\343\203\263\357\274\211.pdf" 	new file:   test/Xu-Xiang-B-Go-revise.zip 	new file:   test/Xu-Xiang-B-Go/main.go 	new file:   "test/Xu-Xiang-B-Go/\343\202\263\343\203\274\343\203\207\343\202\243\343\203\263\343\202\260\343\202\271\343\202\255\343\203\253\343\203\201\343\202\247\343\203\203\343\202\257\343\200\200\345\225\217\351\241\214 \357\274\210B \345\234\260\344\276\241\343\201\256\344\272\210\346\203\263\357\274\211.pdf"

---
+++ test/Xu-Xiang-A-Go-revise/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var H, W, N int
+	fmt.Scanf("%d %d %d", &H, &W, &N)
+	scanner := bufio.NewScanner(os.Stdin)
+	graph := make([][]int, H)
+	for i := range graph {
+		graph[i] = make([]int, W)
+	}
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		arr := strings.Fields(text)
+		hi, _ := strconv.Atoi(arr[0])
+		wi, _ := strconv.Atoi(arr[1])
+		xi, _ := strconv.Atoi(arr[2])
+
+		width := []int{xi, xi + wi - 1}
+
+		for i := 0; i < H; i++ {
+			layerWidth := getWidth(graph[i])
+			if i != H-1 && (layerWidth[0] == -1 || width[1] < layerWidth[0] || width[0] > layerWidth[1]) {
+				continue
+			} else if i == H-1 && (layerWidth[0] == -1 || width[1] < layerWidth[0] || width[0] > layerWidth[1]) {
+				for j := i; j > i-hi; j-- {
+					setWidth(graph[j], width)
+				}
+				break
+			} else {
+				for j := i - 1; j > i-1-hi; j-- {
+					setWidth(graph[j], width)
+				}
+				break
+			}
+		}
+
+		N -= 1
+		if N < 1 {
+			break
+		}
+	}
+
+	stringGraph := convertGraph(graph)
+	printGraph(stringGraph)
+
+}
+
+func getWidth(arr []int) []int {
+	var head, tail int = 0, len(arr) - 1
+	for head <= tail {
+		if arr[head] == 0 {
+			head += 1
+		}
+		if arr[tail] == 0 {
+			tail -= 1
+		} else {
+			break
+		}
+	}
+	if head > tail {
+		return []int{-1}
+	}
+	return []int{head, tail}
+}
+
+func setWidth(arr1 []int, arr2 []int) {
+	for i := arr2[0]; i <= arr2[1]; i++ {
+		arr1[i] = 1
+	}
+}
+
+func printGraph(graph [][]string) {
+	for _, v := range graph {
+		fmt.Println(v)
+	}
+}
+
+func convertGraph(arr [][]int) [][]string {
+	var stringGraph [][]string = make([][]string, len(arr))
+	for i := range stringGraph {
+		stringGraph[i] = make([]string, len(arr[0]))
+		for j := range stringGraph[i] {
+			if arr[i][j] == 1 {
+				stringGraph[i][j] = "#"
+			} else {
+				stringGraph[i][j] = "."
+			}
+		}
+	}
+
+	return stringGraph
+}
