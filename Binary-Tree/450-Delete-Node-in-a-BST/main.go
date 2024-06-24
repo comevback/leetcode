5c0d685c3abddf3d5d05490687d0c55d2b215617
xuxiang <xuxiang5012@gmail.com>
2024-06-24 18:37:33 +0900

    review delete node from bst

--- Binary-Tree/450-Delete-Node-in-a-BST/main.go
+++ Binary-Tree/450-Delete-Node-in-a-BST/main.go
@@ -134,3 +134,82 @@ func deleteNode(root *TreeNode, key int) *TreeNode {
 	// 返回新的根节点
 	return root
 }
+
+// ***************************************************** review 6.23 *************************************************
+// review 6.23
+func deleteNode_623(root *TreeNode, key int) *TreeNode {
+	var deleteNode, deleteParent *TreeNode   // 定义待删除节点及其父节点
+	var replaceNode, replaceParent *TreeNode // 定义替换节点及其父节点
+	current := root                          // 从根节点开始搜索
+
+	// 找到待删除的节点
+	for current != nil {
+		if current.Val == key {
+			deleteNode = current
+			break
+		} else if current.Val < key {
+			deleteParent = current
+			current = current.Right
+		} else if current.Val > key {
+			deleteParent = current
+			current = current.Left
+		}
+	}
+
+	// 如果没有找到待删除的节点，直接返回根节点
+	if current == nil {
+		return root
+	}
+
+	// 情况 1: 待删除节点是叶子节点
+	if deleteNode.Left == nil && deleteNode.Right == nil {
+		if deleteParent == nil {
+			root = nil // 如果待删除节点是根节点，删除后树为空
+		} else {
+			if deleteParent.Left == deleteNode {
+				deleteParent.Left = nil
+			} else {
+				deleteParent.Right = nil
+			}
+		}
+	} else if deleteNode.Right == nil { // 情况 2: 待删除节点只有左子树
+		if deleteParent == nil {
+			temp := deleteNode.Left
+			deleteNode.Left = nil
+			root = temp // 如果待删除节点是根节点，删除后根节点变为其左子节点
+		} else {
+			if deleteParent.Left == deleteNode {
+				deleteParent.Left = deleteNode.Left
+			} else {
+				deleteParent.Right = deleteNode.Left
+			}
+		}
+	} else { // 情况 3: 待删除节点有右子树
+		replaceNode = deleteNode.Right
+
+		// 找到右子树中的最小节点
+		for replaceNode.Left != nil {
+			replaceParent = replaceNode
+			replaceNode = replaceNode.Left
+		}
+
+		// 如果替换节点的父节点不为空，调整替换节点的右子树
+		if replaceParent != nil {
+			replaceParent.Left = replaceNode.Right
+			replaceNode.Right = deleteNode.Right
+		}
+		replaceNode.Left = deleteNode.Left
+
+		if deleteParent == nil {
+			root = replaceNode // 如果待删除节点是根节点，删除后根节点变为替换节点
+		} else {
+			if deleteParent.Left == deleteNode {
+				deleteParent.Left = replaceNode
+			} else {
+				deleteParent.Right = replaceNode
+			}
+		}
+	}
+
+	return root
+}
