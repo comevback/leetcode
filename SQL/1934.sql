506d9138dc8535a6b4f5a605d48b9bcfadfaa4d2
comevback <xuxiang5012@gmail.com>
2025-05-19 16:30:19 +0900

    sql 1934. Confirmation Rate

---
+++ SQL/1934.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+s.user_id,
+IFNULL (ROUND(SUM(c.action = "confirmed")/COUNT(c.action), 2),0) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
