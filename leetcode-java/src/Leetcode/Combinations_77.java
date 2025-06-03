ab31dca38fa9078a452b3fc01fc8b37104660f15
comevback <xuxiang5012@gmail.com>
2025-06-03 18:42:30 +0900

    Combinations_77

---
+++ leetcode-java/src/Leetcode/Combinations_77.java
@@ -0,0 +1,34 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations_77 {
+    public static void main(String[] args) {
+        Combinations_77 app = new Combinations_77();
+        List<List<Integer>> res = app.combine(13,13);
+        for (List<Integer> list : res){
+            for (Integer num : list){
+                System.out.println(num);
+            }
+        }
+    }
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(n, k,1, new ArrayList<>(), res);
+        return res;
+    }
+
+    public void backtrack(int n, int k, int start, List<Integer> path, List<List<Integer>> res){
+        if (path.size() >= k){
+            res.add(new ArrayList<>(path));
+        }else{
+            for (int i = start; i <= n - (k - path.size()) + 1; i++){
+                path.add(i);
+                backtrack(n, k, i+1, path, res);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+}
